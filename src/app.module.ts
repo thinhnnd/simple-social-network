cfc8cc3e7488ca7ae2ae52b9d2b61c04b7b9e4bc
ThinhNie <thinhnienguyen@gmail.com>
2021-04-25 20:42:08 +0700

    feat: add module posts and users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/simple_social_net'), UsersModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
